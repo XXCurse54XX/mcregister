994aa64417640545fcb0006bd579e522d591c4f5
James Slattery <james@slattery.tech>
2017-03-10 22:41:14 -0800

    v0.01

---
+++ js/objects.js
@@ -0,0 +1,5 @@
+var BigMac = {
+  name:"Big Mac",
+  price: 5.40,
+  quantity: 0
+};
---
+++ js/pos.js
@@ -0,0 +1,8 @@
+var total = 0;
+alert(total);
+
+function Buy(item){
+  var price = item.price;
+  var total = total + item.price;
+  alert(total);
+}
