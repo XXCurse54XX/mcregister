1de72238894345fcfd0d22a85e94bed189f13e34
James Slattery <james@slattery.tech>
2017-03-13 20:06:31 -0700

    void() Cost / LI

--- js/pos.js
+++ js/pos.js
@@ -5,10 +5,24 @@ function roundHundred(value){
 }
 
 function Buy(item){
+  var uid = 0;
+  uid += 1;
   var price = item.price;
   total += item.price;
   var fullPrice = (roundHundred(price));
   total = (roundHundred(total));
   document.getElementById("TotalNoTax").innerHTML = total.toFixed(2);
-  $('#printOrder').append("<li>" + item.name + "&nbsp;" +fullPrice.toFixed(2) + "</li>");
+  $('#printOrder').append("<li class='InCartObject'>" + item.name + uid + "<span style='float: right;'>" + fullPrice.toFixed(2) + "</span></li>");
 }
+
+//On Click Event for Items in cart
+// Not Working
+$('.InCartObject').click(function(){
+  $(this).addClass('SelectedInCart');
+  alert("class added");
+});
+
+// Void Line
+function VoidLine(item) {
+  // space
+};
